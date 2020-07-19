23c3c58714dc5ad014a4073d3e4cc69fa4a7d0b0
limxy-wm19 <limxy-wm19@student.tarc.edu.my>
2020-07-19 23:15:14 +0800

    COMMIT BY: XIN YAO

--- Assignment/frmDeleteRentalData2.vb
+++ Assignment/frmDeleteRentalData2.vb
@@ -1,5 +1,6 @@
 ﻿Public Class frmDeleteRentalData2
     Private Sub frmDeleteRentalData2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'EUVUBIRVIEAJVNSNRSKVDKB ZHUO XUAN
+        'GOOD NIGHT
     End Sub
 End Class
