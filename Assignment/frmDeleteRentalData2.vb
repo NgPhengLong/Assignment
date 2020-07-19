445289e75a51cb237de890918866cf4b3ad2023e
user <teezx-wm19@student.tarc.edu.my>
2020-07-19 22:26:51 +0800

    COMMIT BY: ZHUO XUAN

--- Assignment/frmDeleteRentalData2.vb
+++ Assignment/frmDeleteRentalData2.vb
@@ -1,3 +1,5 @@
 ﻿Public Class frmDeleteRentalData2
-
+    Private Sub frmDeleteRentalData2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'EUVUBIRVIEAJVNSNRSKVDKB ZHUO XUAN
+    End Sub
 End Class
